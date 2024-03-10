3b8c37221243509661bcb35ef97c32e5d5f8edd2
Lyanna Butolina <lyana@MacBook-Pro-Lyanna.local>
2024-03-10 14:19:38 +0500

    feat: env file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import 'dotenv/config';
+
 async function bootstrap() {
+  const PORT = process.env.PORT || 4000;
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  await app.listen(PORT, () =>
+    console.log(`Server is listening on port: ${PORT}`),
+  );
 }
 bootstrap();
